9e5a8efc785b4f94238f36f5fd13990b6f970111
lzj123456789 <3150104806@zju.edu.cn>
2018-11-06 23:22:35 +0800

    u

--- tensorflow/webface.py
+++ tensorflow/webface.py
@@ -79,7 +79,7 @@ def read_images(dataset_path, mode, batch_size):
 # Note that a few elements have changed (usage of queues).
 
 # Parameters
-learning_rate = 0.01
+learning_rate = 0.1
 num_steps = 1000
 batch_size = 80
 display_step = 1
